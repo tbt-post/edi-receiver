2c37b1f291c8971423494de16afe7f2827b7565c
Oleg Noga <o.noha@tabata.ua>
2020-03-13 15:08:10 +0200

    extra imports

--- src/edi_receiver/api/handlers/topic.clj
+++ src/edi_receiver/api/handlers/topic.clj
@@ -1,16 +1,13 @@
 (ns edi-receiver.api.handlers.topic
-  (:require [clj-helpers-common.core :as hc]
-            [mount.tools.graph :refer [states-with-deps]]
-            [edi-receiver.config :as config]
+  (:require [mount.tools.graph :refer [states-with-deps]]
             [schema.core :as s]
-            [edi-receiver.upstream :as upstream]
-            [clojure.pprint :refer [pprint]]))
+            [edi-receiver.upstream :as upstream]))
 
 
 (defn post
   {:summary    "Validates and saves message"
    :parameters {:path {(s/optional-key :topic) s/Keyword}}}
   [{{{:keys [topic]} :path} :parameters
-    message :body-params}]
+    message                 :body-params}]
   {:status 200
    :body   (upstream/validate topic message)})
