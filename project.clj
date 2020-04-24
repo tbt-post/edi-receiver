24d6a736dcf9cae44806a76a61eaaba63766276f
source-c <source-c@users.noreply.github.com>
2020-04-24 19:16:52 +0300

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject edi-receiver "0.1.0"
+(defproject edi-receiver "0.2.0"
   :description "EDI receiver"
   :url "https://tbt-post.net/"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
