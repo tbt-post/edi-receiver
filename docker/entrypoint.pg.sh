70d697f8c02de245bd60285e786e943ba6a1448e
Oleg Noga <o.noha@tabata.ua>
2021-01-12 18:29:26 +0200

    updated docker pg to 13

--- docker/entrypoint.pg.sh
+++ docker/entrypoint.pg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pg_ctlcluster 12 main start
+pg_ctlcluster 13 main start
 java -jar target/edi-control-standalone.jar --db pg --deploy && \
 java -jar target/edi-receiver-standalone.jar --db pg
